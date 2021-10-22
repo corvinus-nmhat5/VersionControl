d9e2adb9fa0679c6d9a2b62bcd1bca15ba84f483
corvinus-nmhat5 <abris.agai@stud.uni-corvinus.hu>
2021-10-22 13:25:15 +0200

    5/9

--- UserMaintenance/Value at Risk/Form1.cs
+++ UserMaintenance/Value at Risk/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Value_at_Risk.Entities;
 
 namespace Value_at_Risk
 {
@@ -14,12 +15,24 @@ namespace Value_at_Risk
     {
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
+        List<PortfolioItem> Portfolio;
 
         public Form1()
         {
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10});
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10});
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10});
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
