1f6426720be5a0148025ffa98cbb60da8d79558a
corvinus-nmhat5 <abris.agai@stud.uni-corvinus.hu>
2021-11-12 21:41:22 +0100

    Termékválasztó gombok

--- UserMaintenance/Fejlesztesi_mintak/Form1.Designer.cs
+++ UserMaintenance/Fejlesztesi_mintak/Form1.Designer.cs
@@ -30,14 +30,50 @@ namespace Fejlesztesi_mintak
         private void InitializeComponent()
         {
             this.mainPanel = new System.Windows.Forms.Panel();
+            this.carButton = new System.Windows.Forms.Button();
+            this.ballButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mainPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // mainPanel
             // 
+            this.mainPanel.Controls.Add(this.label1);
+            this.mainPanel.Controls.Add(this.ballButton);
+            this.mainPanel.Controls.Add(this.carButton);
             this.mainPanel.Location = new System.Drawing.Point(0, 1);
             this.mainPanel.Name = "mainPanel";
             this.mainPanel.Size = new System.Drawing.Size(796, 453);
             this.mainPanel.TabIndex = 0;
+            // 
+            // carButton
+            // 
+            this.carButton.Location = new System.Drawing.Point(13, 12);
+            this.carButton.Name = "carButton";
+            this.carButton.Size = new System.Drawing.Size(100, 83);
+            this.carButton.TabIndex = 0;
+            this.carButton.Text = "CAR";
+            this.carButton.UseVisualStyleBackColor = true;
+            this.carButton.Click += new System.EventHandler(this.carButton_Click);
+            // 
+            // ballButton
+            // 
+            this.ballButton.Location = new System.Drawing.Point(119, 12);
+            this.ballButton.Name = "ballButton";
+            this.ballButton.Size = new System.Drawing.Size(100, 83);
+            this.ballButton.TabIndex = 1;
+            this.ballButton.Text = "BALL";
+            this.ballButton.UseVisualStyleBackColor = true;
+            this.ballButton.Click += new System.EventHandler(this.ballButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(226, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Coming next:";
             // 
             // Form1
             // 
@@ -47,6 +83,8 @@ namespace Fejlesztesi_mintak
             this.Controls.Add(this.mainPanel);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -54,6 +92,9 @@ namespace Fejlesztesi_mintak
         #endregion
 
         private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ballButton;
+        private System.Windows.Forms.Button carButton;
     }
 }
 
