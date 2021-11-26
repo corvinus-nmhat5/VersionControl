9f6301ea23be9659e6e9f7b6f1aae3be3df30a51
corvinus-nmhat5 <abris.agai@stud.uni-corvinus.hu>
2021-11-26 13:50:35 +0100

    Teszt projekt létrehozása

---
+++ UserMaintenance/UnitTestExample.Test/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("UnitTestExample.Test")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("UnitTestExample.Test")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("0afeee2f-f6a1-4db9-939c-374963362c75")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
