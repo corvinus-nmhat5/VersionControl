2f651fab6b95fcf104b9386ccea31e45f05ce583
corvinus-nmhat5 <abris.agai@stud.uni-corvinus.hu>
2021-11-26 14:40:40 +0100

    Jelszó validálás teszt

--- UserMaintenance/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UserMaintenance/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -8,9 +8,16 @@ using UnitTestExample.Controllers;
 
 namespace UnitTestExample.Test
 {
-    class AccountControllerTestFixture
+    public class AccountControllerTestFixture
     {
-        [Test]
+        [
+            Test,
+            TestCase("abcd1234", false),
+            TestCase("irf@uni-corvinus", false),
+            TestCase("irf.uni-corvinus.hu", false),
+            TestCase("irf@uni-corvinus.hu", true),
+        ]
+
         public void TestValidateEmail(string email, bool expectedResult)
         {
             // Arrange
@@ -18,6 +25,27 @@ namespace UnitTestExample.Test
 
             // Act
             var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [
+            Test,
+            TestCase("abcdABCD", false),
+            TestCase("ABCD1234", false),
+            TestCase("abcd1234", false),
+            TestCase("Ab1234", false),
+            TestCase("Abcd1234", true)
+        ]
+
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidatePassword(password);
 
             // Assert
             Assert.AreEqual(expectedResult, actualResult);
