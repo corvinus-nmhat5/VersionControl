4d394f34646ff7facdbc522a5be2436d2a6fed71
corvinus-nmhat5 <abris.agai@stud.uni-corvinus.hu>
2021-12-03 21:27:29 +0100

    Szimulációs lépés

--- UserMaintenance/Mikroszimuláció/Form1.cs
+++ UserMaintenance/Mikroszimuláció/Form1.cs
@@ -29,10 +29,9 @@ namespace Mikroszimuláció
             //Szimuláció
             for (int year = 2005; year <= 2024; year++)
             {
-                // Végigmegyünk az összes személyen
                 for (int i = 0; i < Population.Count; i++)
                 {
-                    // Ide jön a szimulációs lépés
+                    SimStep(year, Population[i]);
                 }
 
                 int nbrOfMales = (from x in Population
@@ -107,6 +106,37 @@ namespace Mikroszimuláció
             }
 
             return deathProbabilities;
+        }
+
+        private void SimStep(int year, Person person)
+        {
+            if (!person.IsAlive) return;
+
+            byte age = (byte)(year - person.BirthYear);
+
+            double pDeath = (from x in DeathProbabilities
+                             where x.Gender == person.Gender && x.Age == age
+                             select x.P).FirstOrDefault();
+
+            if (rng.NextDouble() <= pDeath)
+                person.IsAlive = false;
+
+
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                double pBirth = (from x in BirthProbabilities
+                                 where x.Age == age
+                                 select x.P).FirstOrDefault();
+
+                if (rng.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
+            }
         }
     }
 }
